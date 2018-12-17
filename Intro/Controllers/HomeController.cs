ccc01ddc7b59ac30b84e9836d5ce652595765509
shinbaka <geo.wg@hotmail.com>
2018-12-17 18:11:22 -0200

    Add project files.

---
+++ Intro/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa() {
+                PessoaId = 1,
+                Nome = "Pedro",
+                Tipo = "Dev"
+            };
+
+            ViewBag.id = pessoa.PessoaId;
+            ViewBag.nome = pessoa.Nome;
+            ViewBag.tipo = pessoa.Tipo;
+            return View(pessoa);
+        }
+
+        public ActionResult Contatos() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Lista() {
+            return View();
+        }
+    }
+}
---
+++ Intro/Models/Pessoa.cs
@@ -0,0 +1,7 @@
+﻿namespace Intro.Models {
+    public class Pessoa {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
